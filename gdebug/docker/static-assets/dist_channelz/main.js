f3152921c63df549735fa7ffa53c80d7cec42d68
Carl Mastrangelo <notcarl@google.com>
2018-09-26 15:22:06 -0700

    rebuild angular

--- gdebug/docker/static-assets/dist_channelz/main.js
+++ gdebug/docker/static-assets/dist_channelz/main.js
@@ -1476,7 +1476,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /usr/local/google/home/spencerfang/git/grpc-experiments/grpc-zpages/web/channelzui/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /usr/local/google/home/notcarl/git/grpc-experiments/gdebug/web/channelzui/src/main.ts */"./src/main.ts");
 
 
 /***/ })
